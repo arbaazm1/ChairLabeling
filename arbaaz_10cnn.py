69ff922ba1fa02a6d66fe2cbea4e682f2e413e39
akashgokul <akashgokul@berkeley.edu>
2019-11-21 20:28:00 -0800

    changing batch size?

--- arbaaz_10cnn.py
+++ arbaaz_10cnn.py
@@ -37,7 +37,7 @@ class MTurkTrain(Dataset):
     label = img_label_pair[1]
     return img,label
 
-params = {'batch_size': 16,
+params = {'batch_size': 2,
           'shuffle': True,
           'num_workers': 0}
 
